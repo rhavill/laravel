f16f2d1243d02afe52b8f6e3f455d4f12bc2972a
Ricardo Havill <rhavill2002@yahoo.com>
2016-07-06 16:59:05 -0400

    Added delete functionality.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -40,5 +40,7 @@ Route::post('/task', function (Request $request) {
  * Delete Task
  */
 Route::delete('/task/{task}', function (Task $task) {
-    //
+    $task->delete();
+
+    return redirect('/');
 });
