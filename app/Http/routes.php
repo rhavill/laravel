b3a927be32753acea289419634fc8cd2019f0788
Ricardo Havill <rhavill2002@yahoo.com>
2016-07-06 15:36:29 -0400

    Added anonymous route function to create a new task.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,7 +14,22 @@ Route::get('/', function () {
  * Add New Task
  */
 Route::post('/task', function (Request $request) {
-    //
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+
 });
 
 /**
